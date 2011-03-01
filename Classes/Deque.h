ea5ccbcd92cde7b9bab513f611daa0d1889b04c5
Oliver Dodd <ododd@taleo.com>
2011-03-01 17:46:21 -0500

    adding Deque

---
+++ Classes/Deque.h
@@ -0,0 +1,16 @@
+//
+//  Deque.h
+//  CacaoPods
+//
+//  Created by Oliver C Dodd on 2011-03-01.
+//  Copyright 2010 Oliver C Dodd http://01001111.net
+//  Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
+//
+
+#import "Queue.h"
+
+@protocol Deque <Queue>
+
+
+
+@end
